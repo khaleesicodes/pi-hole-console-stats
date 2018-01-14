4ebb37b7340b952cbb2565103ebdaf082e3152a9
Elina Eickstädt <elina.eickstaedt@gmail.com>
2018-01-14 14:38:50 +0100

    Added basic printing of fake test data

---
+++ pi_hole_stats.py
@@ -0,0 +1,10 @@
+# fake Test Data
+IP_ADDRESS = "192.168.x.x"
+CPU_LOAD = "x %"
+MEMORY = "30/100 MB"
+ADS_BLOCKED = "1000"
+
+print("IP ADDRESS :",  IP_ADDRESS)
+print("CPU LOAD :", CPU_LOAD)
+print("MEMORY :", MEMORY)
+print("ADS BLOCKED :", ADS_BLOCKED)
