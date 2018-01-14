eb83c85005693b6b1b19c9d0aa0c3fd736bd7ca9
Elina Eickstädt <elina.eickstaedt@gmail.com>
2018-01-14 17:18:45 +0100

    Refactored table rendering in its own function

--- pi_hole_stats.py
+++ pi_hole_stats.py
@@ -7,24 +7,26 @@ MEMORY = "30/100 MB"
 ADS_BLOCKED = "1000"
 
 
-
 pi_stats_data = [
     ['IP Address', IP_ADDRESS],
     ['CPU Load', CPU_LOAD],
     ['Memory', MEMORY]
 ]
-pi_stats_table = AsciiTable(pi_stats_data)
-pi_stats_table.inner_heading_row_border = False
-pi_stats_table.inner_row_border = True
-pi_stats_table.title = 'rasberry_pi_stats'
 
 pi_hole_stats_data = [
     ['Ads_blocked', ADS_BLOCKED]
 ]
-pi_hole_stats_table = AsciiTable(pi_hole_stats_data)
-pi_hole_stats_table.inner_heading_row_border = False
-pi_hole_stats_table.inner_row_border = True
-pi_hole_stats_table.title = 'pi_hole_stats'
 
-print(pi_stats_table.table)
-print(pi_hole_stats_table.table)
+
+def print_table(data, title):
+    table = AsciiTable(data)
+    table.inner_heading_row_border = False
+    table.inner_row_border = True
+    table.title = title
+    print(table.table)
+
+
+print_table(pi_stats_data, "rasberry_pi_stats")
+print(" ")
+print(" ")
+print_table(pi_hole_stats_data, "pi_hole_stats")
