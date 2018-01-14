c529f60ab1e11433ba3cc93e5513d17199219032
Elina Eickstädt <elina.eickstaedt@gmail.com>
2018-01-14 18:04:39 +0100

    Added actual data from Pi

--- pi_hole_stats.py
+++ pi_hole_stats.py
@@ -1,16 +1,29 @@
 from terminaltables import AsciiTable
+import subprocess
+
+# Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-disp$
+cmd = "hostname -I | cut -d\' \' -f1"
+IP = subprocess.check_output(cmd, shell=True)
+cmd = "hostname"
+HOST = subprocess.check_output(cmd, shell=True)
+cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
+CPU = subprocess.check_output(cmd, shell=True)
+cmd = "free -m | awk 'NR==2{printf \"%s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+MemUsage = subprocess.check_output(cmd, shell=True)
+cmd = "df -h | awk '$NF==\"/\"{printf \"%d/%dGB %s\", $3,$2,$5}'"
+Disk = subprocess.check_output(cmd, shell=True)
+
 
 # fake Test Data
-IP_ADDRESS = "192.168.x.x"
-CPU_LOAD = "x %"
-MEMORY = "30/100 MB"
 ADS_BLOCKED = "1000"
 
 
 pi_stats_data = [
-    ['IP Address', IP_ADDRESS],
-    ['CPU Load', CPU_LOAD],
-    ['Memory', MEMORY]
+    ['IP Address', IP],
+    ['CPU Load', CPU],
+    ['Memory', MemUsage],
+    ['Disk', Disk],
+    ['Host', HOST]
 ]
 
 pi_hole_stats_data = [
